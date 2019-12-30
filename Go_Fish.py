06a91a5e69a79b2c9306c56cf9161ccf51e94d15
Victorli888 <victorli7462@gmail.com>
2019-12-30 15:15:48 -0800

    Added a deck of cards but it doesn't have workable values

--- Go_Fish.py
+++ Go_Fish.py
@@ -1,1 +1,15 @@
+class Card(object):
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+
+values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+suits = ["Spade", "Heart", "Diamond", "Club"]
+
+deck = [Card(value, suit) for value in range values, for suit in range suits]
+
+print(Card(2,"Ace"))
+
+
 
