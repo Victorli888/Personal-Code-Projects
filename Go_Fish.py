9089e9421db1e2c02de292dd2e60a6b3dcc41997
Victorli888 <victorli7462@gmail.com>
2020-01-02 14:13:43 -0800

    Added cards,Deck,Shuffling, and a Player

--- Go_Fish.py
+++ Go_Fish.py
@@ -1,15 +1,55 @@
-class Card(object):
-    def __init__(self, value, suit):
-        self.value = value
+import random
+
+class Card:
+    def __init__(self,suit,val):
         self.suit = suit
+        self.value = val
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s, v))
+
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0,i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+    def drawCard(self):
+        return self.cards.pop()
 
 
-values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-suits = ["Spade", "Heart", "Diamond", "Club"]
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+    def draw(self,deck):
+        self.hand.append(deck.drawCard())
+        return self
 
-deck = [Card(value, suit) for value in range values, for suit in range suits]
+    def showHand(self):
+        for card in self.hand:
+            card.show()
 
-print(Card(2,"Ace"))
+deck = Deck()
+deck.shuffle()
+print("Victor drew...")
+vic = Player("Victor")
+vic.draw(deck)
+vic.showHand()
 
-
+cpu = Player("CPU")
+print("The Computer Drew...")
+cpu.draw(deck)
+cpu.showHand()
 
