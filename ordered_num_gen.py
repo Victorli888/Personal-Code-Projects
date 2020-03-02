dd389ca7b94ebb5dc911553a2905dd33d699bd1b
Victorli888 <victorli7462@gmail.com>
2020-03-01 23:53:48 -0800

    finished ordered num generator

---
+++ ordered_num_gen.py
@@ -0,0 +1,7 @@
+print("How many numbers should be in this list?")
+number = int(input(" "))
+print("How high do you want it to go?")
+max = int(input(" "))
+
+for i in range(1,number+1):
+    print((i), end=' ')
