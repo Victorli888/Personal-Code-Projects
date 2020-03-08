b1dbd044a5a316d1793273a69b84eea12fea5b67
Victorli888 <victorli7462@gmail.com>
2020-03-07 21:41:34 -0800

    Added final touches

--- DnD_damage.py
+++ DnD_damage.py
@@ -1,5 +1,5 @@
 import random, math
-
+line = "--------------------------------"
 class Dice_rolls():
     def D8(self):
         dice6 = (random.randint(1, 8))
@@ -18,17 +18,20 @@ class Dawn_Winters():
     damageG1 = Dice_rolls.D8("Roll")  # weapon allows player Roll 2 dice-8's for Burn damage
     damageG2 = Dice_rolls.D8("Roll")  # this is the 2nd dice-8 roll
     damageB = damageG1 + damageG2
-    print(f"Attack roll: {damage_roll} + {damage_modifier} = {damageA}\nGlaive damage: {damageG1} + {damageG2} = {damageB}\nMastery: 10\nRage: 2")
+
+    print("\nRolling for damage...\n")
+    print(f"Attack roll: {damage_roll} + {damage_modifier} = {damageA}\n"
+          f"Glaive damage: {damageG1} + {damageG2} = {damageB}\nMastery: 10\nRage: 2\n")
     total = damageA + damageB + 10 + 2
-    print(f"Total Damage: {total}")
+    print(line)
+    print(f"Standard Damage: {total - 12}")
+    print(f"Raging Mastery Damage: {total}")
+    print(line)
     ans = input("\n*** Press Enter for Next Damage Roll or type [end] to end ***\n")
     if ans == "End" or ans == "end":
         pass
     else:
         Dawn_Winters.savage_mastery("roll")
-
-
-
 
 
 Dawn_Winters.savage_mastery("roll")
