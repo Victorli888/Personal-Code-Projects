874b81e3b7503884e446265ad837f09c9fd0fc01
Victorli888 <victorli7462@gmail.com>
2020-04-28 01:52:29 -0700

    Mock Inventory #2 using Python Counter Arithmetic

---
+++ Game_Inventory2.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+"""
+I need to complete two objectives:
+1. Create an accessible inventory system
+2. Increase and decrease based on player input
+"""
+
+
+# Player will need to use items in his inventory to complete this inventory
+
+def bag_system(inventory, ans):
+    if ans == "A":
+        invb = Counter({'potions': 1, 'pokeball': 0})
+        inventory = inventory - invb
+        print(f"current inventory: {inventory}")
+        return inventory
+    elif ans == "B":
+        print("failed")
+        invb = Counter({'potions': 0, 'pokeball': 1})
+        inventory = inventory - invb
+        print(f"current inventory: {inventory}")
+        return inventory
+    else:
+        binventory = inventory
+        print("not a valid answer please try again........")
+        ans = input("> ")
+        bag_system(binventory, ans)
+
+
+inventory = Counter({ 'potions':10 , 'pokeball':6})
+
+print(f"current inventory: {inventory}")
+print("You are battling Pidgy, would you like to [A] Heal your Pokemon or [B] Capture the Pidgey?")
+ans = input("> ")
+inventory = bag_system(inventory, ans)
