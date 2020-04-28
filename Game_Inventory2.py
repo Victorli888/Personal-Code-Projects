390b8c4b0b35cfe0eae35c2c0c30dbf14aa0ae88
Victorli888 <victorli7462@gmail.com>
2020-04-28 11:25:48 -0700

    Inventory in Working status, saves player inventory based on player input

--- Game_Inventory2.py
+++ Game_Inventory2.py
@@ -5,35 +5,46 @@ I need to complete two objectives:
 1. Create an accessible inventory system
 2. Increase and decrease based on player input
 
-1.0 I have issues with running and saving the usable values for Inventory for multible iterations. It can only complete
-one iteration before running into issues.
+Notes:
+Inventory subtraction varaible = inv_s In this method we have a counter hash table
+that tracks the key-value pair with value being the quantity of that item. we use 
+inv_s to subtract a quantity from the value when the item is used.
+
+Else statement simply closes the operation instead of looping because I need a statement
+in place to show that we can go back to the previous menu.
+
+Time Complexity O(N)
+Space Complexity O(1)
 """
 
 
 # Player will need to use items in his inventory to complete this inventory
 
-def bag_system(inventory, ans):
-    if ans == "A":
-        invb = Counter({'potions': 1, 'pokeball': 0})
-        inventory = inventory - invb
-        print(f"current inventory: {inventory}")
-        return inventory
-    elif ans == "B":
-        print("failed")
-        invb = Counter({'potions': 0, 'pokeball': 1})
-        inventory = inventory - invb
-        print(f"current inventory: {inventory}")
-        return inventory
+def bag_system(curr_inventory, p_input):
+    if p_input == "A":
+        inv_s = Counter({'potions': 1, 'pokeball': 0})
+        curr_inventory = curr_inventory - inv_s
+        print("Using Potion...")
+        print(f"current inventory: {curr_inventory}")
+        return curr_inventory
+    elif p_input == "B":
+        print("Using Pokeball...")
+        inv_s = Counter({'potions': 0, 'pokeball': 1})
+        curr_inventory = curr_inventory - inv_s
+        print(f"current inventory: {curr_inventory}")
+        return curr_inventory
     else:
-        binventory = inventory
-        print("not a valid answer please try again........")
-        ans = input("> ")
-        bag_system(binventory, ans)
+        print("not a valid answer exiting operation")
+        return curr_inventory
 
 
-inventory = Counter({ 'potions':10 , 'pokeball':6})
+inventory = Counter({'potions': 10, 'pokeball': 6})
 
 print(f"current inventory: {inventory}")
 print("You are battling Pidgy, would you like to [A] Heal your Pokemon or [B] Capture the Pidgey?")
 ans = input("> ")
 inventory = bag_system(inventory, ans)
+print("You are battling Pidgy, would you like to [A] Heal your Pokemon or [B] Capture the Pidgey?")
+ans = input("> ")
+inventory = bag_system(inventory, ans)
+
