5820847720419ed88918e6c839093c739df13a6c
Victorli888 <victorli7462@gmail.com>
2020-04-28 01:54:09 -0700

    added comments

--- Game_Inventory.py
+++ Game_Inventory.py
@@ -1,3 +1,9 @@
+"""
+The issue with this method is I have a counter and it does add up but I don't have something in place if the refuse
+the item. I will need to look back on this and see what can be salvaged from this method
+"""
+
+
 def event_text(quantity,item):
     print(f"Congratulations! you have found {quantity} {item}.")
     print("Would you like to add this to your inventory? Please say [Y] or [N]")
