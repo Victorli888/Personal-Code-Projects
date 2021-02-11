2dc03ce6e84629e174db3317b350888e888efb31
Victor <victorli7462@gmail.com>
2021-02-10 17:36:16 -0800

    Results.Write() writes to appropiate %

--- xldf_diff_tool.py
+++ xldf_diff_tool.py
@@ -1,14 +1,13 @@
 from xml.dom import minidom
 from os import listdir
-from os.path import isfile, join
+from os.path import join
 
 
 def file_path_array(directory_path):
     full_path = []
     for file in listdir(directory_path):
-        if isfile(file):
-            fpath = join(directory_path, file)
-            full_path.append(fpath)
+        fpath = join(directory_path, file)
+        full_path.append(fpath)
     return full_path
 
 
@@ -33,6 +32,43 @@ def PEV(file, TagName, Component):  # Parse Element Value
 
     return arr
 
+def pop_check(A, B, C, D):   # Unused for now
+    popped_89 = A.pop(5)
+    if popped_89 != "89 Percent":
+        Results.write(f" Pool Percentage {files89[i]} Fails to say 89%\n")
+    popped_90 = A.pop(5)
+    if popped_90 != "90 Percent":
+        Results.write(f" Pool Percentage {files89[i]} Fails to say 90%\n")
+    popped_91 = A.pop(5)
+    if popped_91 != "91 Percent":
+        Results.write(f" Pool Percentage {files89[i]} Fails to say 91%\n")
+    popped_92 = A.pop(5)
+    if popped_92 != "92 Percent":
+        Results.write(f" Pool Percentage {files89[i]} Fails to say 92%\n")
+    return A, B, C, D
+
+def compare(A, B, C, D, component_compared):
+    if A == B:
+        print(f"90% {component_compared} Section Passed!")
+    else:
+        print(f"90% {component_compared} Section failed")
+        Results.write(f"\n90% {component_compared} Section failed, Please Verify this section")
+    if A == C:
+        print(f"91% {component_compared} Section Passed!")
+    else:
+        print(f"91% {component_compared} Section failed")
+        Results.write(f"\n91% {component_compared} Section failed, Please Verify this section")
+    if A == D:
+        print(f"92% {component_compared} Section Passed!")
+    else:
+        print(f"92% {component_compared} Section failed")
+        Results.write(f"\n92% {component_compared} Section failed, Please Verify this section")
+
+
+
+# Create Text File
+Results = open("xldf_compare_results.txt", "w+")
+
 # Recieve XLDF locations
 xldf_89 = input("Location of 89% compare XLDF: ")  # XLDF Location on your local machine
 xldf_90 = input("Location of 90% compare XLDF: ")  # XLDF Location on your local machine
@@ -45,10 +81,152 @@ files90 = file_path_array(xldf_90)
 files91 = file_path_array(xldf_91)
 files92 = file_path_array(xldf_92)
 
-# Parse data from first XLDF
-for i in len(files89):
+# Parse data from first XLDF starting with bet pool 1
+for i in range(len(files89)):
+    bet_pool = i+1
+    print(f"Working...  {bet_pool}/{len(files89)} ...")
+    Results.write(f"\n\nComparing with {files89[i]}")
+    # <GameDefinition>
+    game_def = PEV(files89[i], "GameDefinition", "Version")
+    B = PEV(files90[i], "GameDefinition", "Version")
+    C = PEV(files91[i], "GameDefinition", "Version")
+    D = PEV(files92[i], "GameDefinition", "Version")
+    compare(game_def, B, C, D, "GameDefinition")
+    print(1)
     game_name = PTC(files89[i], "GameName")
+    B = PTC(files90[i], "GameName")
+    C = PTC(files91[i], "GameName")
+    D = PTC(files92[i], "GameName")
+    compare(game_name, B, C, D, "GameName")
+    print(2)
     index_name = PTC(files89[i], "IndexName")
+    B = PTC(files90[i], "IndexName")
+    C = PTC(files91[i], "IndexName")
+    D = PTC(files92[i], "IndexName")
+    compare(index_name, B, C, D, "IndexName")
+    print(3)
     friendly_name = PTC(files89[i], "FriendlyName")
+    B = PTC(files90[i], "FriendlyName")
+    C = PTC(files91[i], "FriendlyName")
+    D = PTC(files92[i], "FriendlyName")
+    Results.write("\nConfirm only the Percentages are different")
+    Results.write(f"\n{friendly_name}\n{B}\n{C}\n{D}\n")
+
+
+
+    print(4)
+    Manufacturer = PTC(files89[i], "Manufacturer")
+    B = PTC(files90[i], "Manufacturer")
+    C = PTC(files91[i], "Manufacturer")
+    D = PTC(files92[i], "Manufacturer")
+    compare(Manufacturer, B, C, D, "Manufacturer")
+    print(5)
+    Credits = PTC(files89[i], "Credits")
+    B = PTC(files90[i], "Credits")
+    C = PTC(files91[i], "Credits")
+    D = PTC(files92[i], "Credits")
+    compare(Credits, B, C, D, "Credits")
+    print(6)
+    Denomination = PTC(files89[i], "Denomination")
+    B = PTC(files90[i], "Denomination")
+    C = PTC(files91[i], "Denomination")
+    D = PTC(files92[i], "Denomination")
+    compare(Denomination, B, C, D, "Denomination")
+    print(7)
+    PayTableIndex = PTC(files89[i], "PayTableIndex")
+    B = PTC(files90[i], "PayTableIndex")
+    C = PTC(files91[i], "PayTableIndex")
+    D = PTC(files92[i], "PayTableIndex")
+    compare(PayTableIndex, B, C, D, "PayTableIndex")
+    print(8)
+    GameUPCNumber = PTC(files89[i], "GameUPCNumber")
+    B = PTC(files90[i], "GameUPCNumber")
+    C = PTC(files91[i], "GameUPCNumber")
+    D = PTC(files92[i], "GameUPCNumber")
+    compare(GameUPCNumber, B, C, D, "GameUPCNumber")
+    print(9)
+    # <Progressive/> Components
+    Pool_Name = PEV(files89[i], "Pool", "Name")
+    B = PEV(files90[i], "Pool", "Name")
+    C = PEV(files91[i], "Pool", "Name")
+    D = PEV(files92[i], "Pool", "Name")
+    compare(Pool_Name, B, C, D, "Pool_Name")
+    print(10)
+
+    Pool_Type = PEV(files89[i], "Pool", "Type")
+    B = PEV(files90[i], "Pool", "Type")
+    C = PEV(files91[i], "Pool", "Type")
+    D = PEV(files92[i], "Pool", "Type")
+    compare(Pool_Type, B, C, D, "Pool_Type")
+    print(11)
+
+    Pool_Pcnt = PEV(files89[i], "Pool", "Percentage")
+    B = PEV(files90[i], "Pool", "Percentage")
+    C = PEV(files91[i], "Pool", "Percentage")
+    D = PEV(files92[i], "Pool", "Percentage")
+    compare(Pool_Pcnt, B, C, D, "Pool_Pcnt")
+    print(12)
+
+    Pool_Seed = PEV(files89[i], "Pool", "Seed")
+    B = PEV(files90[i], "Pool", "Seed")
+    C = PEV(files91[i], "Pool", "Seed")
+    D = PEV(files92[i], "Pool", "Seed")
+    compare(Pool_Seed, B, C, D, "Pool_Seed")
+    print(13)
+
+    Trigger_Index = PEV(files89[i], "Trigger", "Index")
+    B = PEV(files90[i], "Trigger", "Index")
+    C = PEV(files91[i], "Trigger", "Index")
+    D = PEV(files92[i], "Trigger", "Index")
+    compare(Trigger_Index, B, C, D, "Trigger_Index")
+    print(14)
+
+    # <Prizes/> Components
+    Prizes_TO = PEV(files89[i], "Prizes", "TotalOutcomes")
+    B = PEV(files90[i], "Prizes", "TotalOutcomes")
+    C = PEV(files91[i], "Prizes", "TotalOutcomes")
+    D = PEV(files92[i], "Prizes", "TotalOutcomes")
+    compare(Prizes_TO, B, C, D, "Prizes_TotalOutcomes")
+    print(15)
+
+    Prizes_TS = PEV(files89[i], "Prizes", "TotalSubsets")
+    B = PEV(files90[i], "Prizes", "TotalSubsets")
+    C = PEV(files91[i], "Prizes", "TotalSubsets")
+    D = PEV(files92[i], "Prizes", "TotalSubsets")
+    compare(Prizes_TS, B, C, D, "Prizes_TotalSubsets")
+    print(16)
+    # <Prize/> Components
+
+    Prize_Value = PEV(files89[i], "Prize", "Value")
+    B = PEV(files90[i], "Prize", "Value")
+    C = PEV(files91[i], "Prize", "Value")
+    D = PEV(files92[i], "Prize", "Value")
+    compare(Prize_Value, B, C, D, "Prize_Value")
+    print(17)
+
+    Prize_Index = PEV(files89[i], "Prize", "Index")
+    B = PEV(files90[i], "Prize", "Index")
+    C = PEV(files91[i], "Prize", "Index")
+    D = PEV(files92[i], "Prize", "Index")
+    compare(Prize_Index, B, C, D, "Prize_Index")
+    print(18)
+
+    # Prize_Freq = PEV(files89[0], "Prize", "Frequency") This component should be different
+
+    Prize_EI = PEV(files89[i], "Prize", "ExtendedInfo")
+    B = PEV(files90[i], "Prize", "ExtendedInfo")
+    C = PEV(files91[i], "Prize", "ExtendedInfo")
+    D = PEV(files92[i], "Prize", "ExtendedInfo")
+    compare(Prize_EI, B, C, D, "Prize_ExtendedInfo")
+
+    print(19)
+
+
+
+
+
+
+
+
 
 
